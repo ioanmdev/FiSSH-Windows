717da340a5c81bc45ff86712be38238f2daded0b
Ioan Moldovan <ioanm@tuta.io>
2019-02-10 09:54:05 +0200

    Initial release

---
+++ FiSSH-Windows/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FiSSH_Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter Error = Console.Error;
+            try
+            {
+                // Locate FiSSH certificate
+                X509Certificate2 fissh_cert = null;
+                X509Store x509Store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                x509Store.Open(OpenFlags.ReadOnly);
+
+                foreach (X509Certificate2 x509Certificate in x509Store.Certificates)
+                {
+                    if (x509Certificate.Subject.Contains("CN=FiSSH"))
+                    {
+                        fissh_cert = x509Certificate;
+                        Error.WriteLine("FiSSH Certificate Loaded!");
+                        break;
+                    }
+                }
+
+                if (fissh_cert == null) throw new Exception("Unable to find FiSSH certificate");
+                if (!fissh_cert.HasPrivateKey) throw new Exception("Unable to load FiSSH private key");
+
+                TcpListener tcpListener = new TcpListener(IPAddress.Any, 2222);
+                tcpListener.Start();
+                Error.WriteLine("Please authorize the SSH connection using the FiSSH app on your phone.");
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                SslStream sslStream = new SslStream(tcpClient.GetStream());
+                sslStream.AuthenticateAsServer(fissh_cert, false, System.Security.Authentication.SslProtocols.Tls | System.Security.Authentication.SslProtocols.Tls11 | System.Security.Authentication.SslProtocols.Tls12, false);
+                sslStream.ReadTimeout = 4000;
+                sslStream.WriteTimeout = 4000;
+                StreamReader streamReader = new StreamReader(sslStream);
+                Error.WriteLine("Authorization received. Relaying authorization to OpenSSH.");
+                Console.WriteLine(streamReader.ReadLine());
+                sslStream.Close();
+                tcpListener.Stop();
+            }
+            catch (Exception ex)
+            {
+                Error.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}

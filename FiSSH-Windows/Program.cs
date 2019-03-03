9c80bd2e46a007e97077dfd50a62d1c3d13f4504
Ioan Moldovan <ioanm@tuta.io>
2019-03-03 11:15:54 +0200

    Fix issues with interrupted handshake

--- FiSSH-Windows/Program.cs
+++ FiSSH-Windows/Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace FiSSH_Windows
 {
@@ -37,9 +38,17 @@ namespace FiSSH_Windows
                 Error.WriteLine("Please authorize the SSH connection using the FiSSH app on your phone.");
                 TcpClient tcpClient = tcpListener.AcceptTcpClient();
                 SslStream sslStream = new SslStream(tcpClient.GetStream());
-                sslStream.AuthenticateAsServer(fissh_cert, false, System.Security.Authentication.SslProtocols.Tls | System.Security.Authentication.SslProtocols.Tls11 | System.Security.Authentication.SslProtocols.Tls12, false);
-                sslStream.ReadTimeout = 4000;
-                sslStream.WriteTimeout = 4000;
+                
+                IAsyncResult secureConnect = sslStream.BeginAuthenticateAsServer(fissh_cert, false, System.Security.Authentication.SslProtocols.Tls | System.Security.Authentication.SslProtocols.Tls11 | System.Security.Authentication.SslProtocols.Tls12, false, null, null);
+                
+                if (!secureConnect.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(5), false))
+                {
+                    sslStream.Close();
+                    throw new Exception("Handshake failed due to timeout.\n\nNOTE: If you are prompted on your phone to approve the certificate, then this error is NORMAL, verify the certificate fingerprint as instructed and run FiSSH on your PC again after approving the certificate.\n\n");
+                }
+
+                sslStream.EndAuthenticateAsServer(secureConnect);
+
                 StreamReader streamReader = new StreamReader(sslStream);
                 Error.WriteLine("Authorization received. Relaying authorization to OpenSSH.");
                 Console.WriteLine(streamReader.ReadLine());
